ff470616b59c7c612242a2daafa881d952578559
Enrique Hilst <enrique.hilst@gmail.com>
2020-06-17 10:44:35 -0500

    added questions to make 5

--- js/app.js
+++ js/app.js
@@ -11,7 +11,6 @@ function firstQuestion(){
   return lowerMyLocation;
 }
 
-
 // Check input and respond to location question
 var firstAnswer = firstQuestion();
 if (firstAnswer === 'yes'){
@@ -33,7 +32,6 @@ function secondQuestion(){
   return lowerMyDog;
 }
 
-
 // Check input and respond to dog question
 var secondAnswer = secondQuestion();
 if (secondAnswer === 'yes'){
@@ -45,6 +43,7 @@ if (secondAnswer === 'yes'){
   secondQuestion();
 }
 
+
 // Third quesition: ask user if I've travelled outside of the country
 // Function to ask third question and convert answer to lower case
 function thirdQuestion(){
@@ -54,13 +53,54 @@ function thirdQuestion(){
   return lowerMyTravel;
 }
 
-
 // Check input and respond to travel question
 var thirdAnswer = thirdQuestion();
 if (thirdAnswer === 'yes'){
   alert ('True! I have visted 11 countries.');
 } else if (thirdAnswer === 'no'){
   alert ('False! I have visted 11 countries.');
+} else {
+  alert ('You must enter yes or no.');
+  secondQuestion();
+}
+
+
+// Fourth quesition: ask user if I fly on a private jet
+// Function to ask foruth question and convert answer to lower case
+function fourthQuestion(){
+  var myJet = prompt('Do I normally fly on a private jet?','yes or no');
+  var lowerMyJet = myJet.toLowerCase();
+  console.log(lowerMyJet);
+  return lowerMyJet;
+}
+
+// Check input and respond to travel question
+var fourthAnswer = fourthQuestion();
+if (fourthAnswer === 'yes'){
+  alert ('False! I don\'t personally know anyone that has a private jet.');
+} else if (fourthAnswer === 'no'){
+  alert ('True! I don\'t personally know anyone that has a private jet.');
+} else {
+  alert ('You must enter yes or no.');
+  secondQuestion();
+}
+
+
+// Fifth quesition: ask user if I have any siblings
+// Function to ask fifth question and convert answer to lower case
+function fifthQuestion(){
+  var mySister = prompt('Do I have any siblings?','yes or no');
+  var lowerMySister = mySister.toLowerCase();
+  console.log(lowerMySister);
+  return lowerMySister;
+}
+
+// Check input and respond to travel question
+var fifthAnswer = fifthQuestion();
+if (fifthAnswer === 'yes'){
+  alert ('True! I have an older sister. She lives in Australia.');
+} else if (fifthAnswer === 'no'){
+  alert ('False! I have an older sister. She lives in Australia.');
 } else {
   alert ('You must enter yes or no.');
   secondQuestion();
